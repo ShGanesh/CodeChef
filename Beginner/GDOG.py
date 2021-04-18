14bf59422efa7661bff98751c57cc4be4b14a495
ShGanesh <54804789+ShGanesh@users.noreply.github.com>
2021-04-18 20:25:13 +0100

    Create GDOG.py

---
+++ Beginner/GDOG.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    N, K = map(int, input().split())
+    rem = 0
+    for i in range(1, K+1):
+        if N%i > rem:
+            rem = N%i
+    print(rem)
