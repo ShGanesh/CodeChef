e63bdcdfed1754c709ef2abb81e203ebf9cda4e3
ShGanesh <54804789+ShGanesh@users.noreply.github.com>
2021-04-18 20:12:36 +0100

    Create TheLeadGame.py

---
+++ Beginner/TheLeadGame.py
@@ -0,0 +1,17 @@
+lead = 0
+winner = 0
+a = 0
+b = 0
+
+for i in range(int(input())):
+    x, y = map(int, input().split())
+    a += x
+    b += y
+    if a - b > lead:
+        winner = 1
+        lead = a - b
+    elif b - a > lead:
+        winner = 2
+        lead = b - a
+
+print(winner, lead)
